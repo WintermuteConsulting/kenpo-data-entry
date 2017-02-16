1a0c20d44ff2308d98c7c71fead76576892649bf
Andrew Falconer <falconera@users.noreply.github.com>
2017-02-16 11:17:57 -0600

    Rename helpful.js -> stringifyID.js, refactoring stringify and server.js to meet lint standards

--- src/server.js
+++ src/server.js
@@ -1,25 +1,24 @@
 // Modules
-var { MongoClient: mongoc, ObjectID } = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-const R = require('ramda');
-const { stringifyID } = require('./helpful');
-const { open: openURL } = require('openurl');
+import { MongoClient as mongoc, ObjectID } from 'mongodb';
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import R from 'ramda';
+import stringifyID from './stringifyID';
 
 // Express configuration
-var app = express();
+const app = express();
 app.use(bodyParser.json());
 
 // Server configuration
 const url = 'mongodb://localhost:27017/mykenpo';
 
 mongoc.connect(url)
-.then(db => {
-
+.then((db) => {
   // Close the DB connection if the server is interrupted
   process.on('SIGINT', () => {
-    console.log("\nbye");
+    // eslint-disable-next-line no-console
+    console.log('\nbye');
     db.close();
     process.exit();
   });
@@ -27,59 +26,79 @@ mongoc.connect(url)
   // Install routers
   const publicDir = path.join(__dirname, 'public');
   app.use(express.static(publicDir));
-  app.get('/', (req, res, next) => {
+  app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(publicDir, 'mock.html'));
   });
 
   app.route('/:collection/:id?')
-  .all((req, res, next) => {
+  .all((req, res) => {
     const { collection: name, id } = req.params;
     const collection = db.collection(name);
     if (id !== undefined) {
       try {
-        let query = { _id: ObjectID(id) }
+        const query = { _id: ObjectID(id) };
         switch (req.method) {
-            case 'GET':
-              collection.findOne(query)
-              .then(doc => { (doc !== null) ? res.status(200).json(stringifyID(doc)) : res.sendStatus(404) })
-              .catch(err => res.sendStatus(500));
-              break;
+          case 'GET':
+            collection.findOne(query)
+              .then((doc) => {
+                if (doc !== null) {
+                  res.status(200).json(stringifyID(doc));
+                } else {
+                  res.sendStatus(404);
+                }
+              })
+              .catch(() => res.sendStatus(500));
+            break;
 
-            case 'PUT':
-              collection.findOneAndReplace(query, req.body)
-              .then(result => { (result.value !== null) ? res.sendStatus(200) : res.sendStatus(404) })
-              .catch(err => res.sendStatus(500));
-              break;
+          case 'PUT':
+            collection.findOneAndReplace(query, req.body)
+              .then((result) => {
+                if (result.value !== null) {
+                  res.sendStatus(200);
+                } else {
+                  res.sendStatus(404);
+                }
+              })
+              .catch(() => res.sendStatus(500));
+            break;
 
-            case 'DELETE':
-              collection.findOneAndDelete(query)
-              .then(result => { (result.value !== null) ? res.sendStatus(200) : res.sendStatus(404) })
-              .catch(err => res.sendStatus(500));
-              break;
+          case 'DELETE':
+            collection.findOneAndDelete(query)
+              .then((result) => {
+                if (result.value !== null) {
+                  res.sendStatus(200);
+                } else {
+                  res.sendStatus(404);
+                }
+              })
+              .catch(() => res.sendStatus(500));
+            break;
 
-            default:
+          default:
               // Unsupported method
-              res.sendStatus(404);
+            res.sendStatus(404);
         }
-      }
-      catch (e) {
+      } catch (e) {
         // Invalid `id`
         res.sendStatus(404);
       }
-    }
-    else {
+    } else {
       switch (req.method) {
         case 'GET':
           collection.find().toArray()
           .then(R.map(stringifyID))
           .then(docs => res.status(200).json(docs))
-          .catch(err => res.sendStatus(500))
+          .catch(() => res.sendStatus(500));
           break;
 
         case 'POST':
           collection.insertOne(req.body, (err, result) => {
-            (err === null) ? res.set('Location', `${result.insertedId}`).sendStatus(201) : res.sendStatus(500)
-          })
+            if (err === null) {
+              res.set('Location', `${result.insertedId}`).sendStatus(201);
+            } else {
+              res.sendStatus(500);
+            }
+          });
           break;
 
         default:
@@ -87,14 +106,16 @@ mongoc.connect(url)
           res.sendStatus(404);
       }
     }
-  })
+  });
 
   // Start the server
   app.listen(3000, () => {
+    // eslint-disable-next-line no-console
     console.log('The server is running at http://localhost:3000/');
   });
 })
-.catch(err => {
+.catch((err) => {
+  // eslint-disable-next-line no-console
   console.log(`[!] ${err}. Check that mongod is running!`);
   process.exitCode = 1;
 });
