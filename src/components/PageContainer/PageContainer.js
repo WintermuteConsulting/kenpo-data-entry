b4b63bb6b2d1b5253e24e354a0083f4ba8dd2a2a
Andrew Falconer <falconera@users.noreply.github.com>
2017-02-20 18:42:22 -0600

    Updating PageContainer to reference Workspace

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './PageContainer.css';
 import Header from '../Header/Header';
+import Workspace from '../Workspace/Workspace';
 
 function PageContainer() {
   return (
     <div className={styles.pageContainer}>
       <Header />
+      <Workspace />
     </div>
   );
 }
--- src/components/PageContainer/PageContainer.test.js
+++ src/components/PageContainer/PageContainer.test.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import PageContainer from './PageContainer';
 import Header from '../Header/Header';
+import Workspace from '../Workspace/Workspace';
 
-test('page container renders header', (t) => {
+test('page container renders header and workspace', (t) => {
   const wrapper = shallow(<PageContainer />);
   t.assert(wrapper.containsMatchingElement(<Header />), 'missing Header component');
+  t.assert(wrapper.containsMatchingElement(<Workspace />), 'missing Workspace component');
   t.end();
 });
