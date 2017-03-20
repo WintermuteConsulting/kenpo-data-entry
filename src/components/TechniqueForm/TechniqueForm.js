e1d710be151cd59718296ca0d1213e2b0ecb7ace
Andrew Falconer <falconera@users.noreply.github.com>
2017-03-20 14:46:17 -0500

    Adding TechniqueForm component

---
+++ src/components/TechniqueForm/TechniqueForm.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import hash from 'string-hash';
+import TextInput from '../TextInput/TextInput';
+
+// Labels should identify their corresponding element with a 'for' attribute.
+// This requires an 'id' applied to the form element, but we don't want the 'id'
+// to collide with anything outside this component, so we hash the field name
+// with the form name.
+export const ids = {
+  title: hash('technique-title').toString(),
+  attack: hash('technique-attack').toString(),
+};
+
+class TechniqueForm extends React.Component {
+  constructor(props) {
+    super(props);
+    const data = props.data;
+    this.state = { title: data.title, attack: data.attack };
+  }
+
+  makeHandler(key) {
+    return event => this.setState({ [key]: event.target.value });
+  }
+
+  render() {
+    const onClick = handler => ((e) => { e.preventDefault(); handler(this.state); });
+    return (
+      <form>
+        <button type="submit" onClick={onClick(this.props.onSubmit)}>Save</button>
+        <fieldset key={ids.title}>
+          <label htmlFor={ids.title}>title</label>
+          <TextInput id={ids.title} value={this.state.title} onChange={this.makeHandler('title')} />
+        </fieldset>
+        <fieldset key={ids.attack}>
+          <label htmlFor={ids.attack}>attack</label>
+          <TextInput id={ids.attack} value={this.state.attack} onChange={this.makeHandler('attack')} />
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+TechniqueForm.propTypes = {
+  data: React.PropTypes.shape({
+    title: React.PropTypes.string,
+    attack: React.PropTypes.string,
+  }),
+  onSubmit: React.PropTypes.func,
+};
+
+TechniqueForm.defaultProps = {
+  data: { title: '', attack: '' },
+  onSubmit: undefined,
+};
+
+export default TechniqueForm;
---
+++ src/components/TechniqueForm/TechniqueForm.test.js
@@ -0,0 +1,57 @@
+import test from 'tape';
+import React from 'react';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import TechniqueForm, { ids } from './TechniqueForm';
+
+test('technique form renders a form with title, attack fields and submit', (t) => {
+  const wrapper = shallow(<TechniqueForm />);
+  t.is(wrapper.find('form').length, 1, 'missing form element');
+  t.is(wrapper.find('button[type="submit"]').length, 1, 'missing submit button');
+  t.is(wrapper.find(`#${ids.title}`).length, 1, 'missing title input');
+  t.assert(wrapper.find(`#${ids.attack}`).length, 1, 'missing attack input');
+  t.end();
+});
+
+test('technique form is empty if not supplied with data', (t) => {
+  const wrapper = shallow(<TechniqueForm />);
+  t.is(wrapper.find(`#${ids.title}`).prop('value'), '', 'title is not empty');
+  t.is(wrapper.find(`#${ids.attack}`).prop('value'), '', 'attack is not empty');
+  t.end();
+});
+
+test('technique form applies initial data to inputs', (t) => {
+  const mock = {
+    title: 'Lurking Tiger Strike',
+    attack: 'front left do-si-do',
+  };
+  const wrapper = shallow(<TechniqueForm data={mock} />);
+  t.is(wrapper.find(`#${ids.title}`).prop('value'), mock.title, 'title is not applied');
+  t.is(wrapper.find(`#${ids.attack}`).prop('value'), mock.attack, 'attack is not applied');
+  t.end();
+});
+
+test('technique form can update its inputs', (t) => {
+  const mock = {
+    title: 'Lurking',
+    attack: 'full',
+  };
+  const expected = {
+    title: `${mock.title} Tiger`,
+    attack: `${mock.attack} nelson`,
+  };
+  const wrapper = shallow(<TechniqueForm data={mock} />);
+  wrapper.find(`#${ids.title}`).simulate('change', { target: { value: expected.title } });
+  wrapper.find(`#${ids.attack}`).simulate('change', { target: { value: expected.attack } });
+  t.is(wrapper.find(`#${ids.title}`).prop('value'), expected.title, 'title is not applied');
+  t.is(wrapper.find(`#${ids.attack}`).prop('value'), expected.attack, 'attack is not applied');
+  t.end();
+});
+
+test('technique form uses its prop submit handler', (t) => {
+  const callback = sinon.spy();
+  const wrapper = shallow(<TechniqueForm onSubmit={callback} />);
+  wrapper.find('button[type="submit"]').simulate('click', { preventDefault() {} });
+  t.assert(callback.called, 'submit handler not called');
+  t.end();
+});
