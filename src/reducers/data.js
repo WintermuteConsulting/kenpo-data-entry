7e0633a9f09c904dd78dc38e348971e5d2f4f1f5
Andrew Falconer <falconera@users.noreply.github.com>
2017-03-20 20:04:29 -0500

    Adding UPDATE_ITEM case to data reducer

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,5 +1,8 @@
 function data(state = {}, action) {
   switch (action.type) {
+    case 'UPDATE_ITEM':
+      return Object.assign({}, state, { [action.id]: action.datum });
+
     default:
       return state;
   }
