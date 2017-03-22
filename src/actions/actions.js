0677f7117dd76769e3cf7422059fc3033f629339
Andrew Falconer <falconera@users.noreply.github.com>
2017-03-22 14:42:28 -0500

    Adding create/delete actions in actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,2 +1,4 @@
 export const setSelection = id => ({ type: 'SET_SELECTION', id });
+export const createItem = () => ({ type: 'CREATE_ITEM' });
 export const updateItem = (id, datum) => ({ type: 'UPDATE_ITEM', id, datum });
+export const deleteItem = () => ({ type: 'DELETE_ITEM' });
