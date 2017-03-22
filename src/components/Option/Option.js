f6796b1dc43fdcbcca9615d09ee8d0d538cd13ae
Andrew Falconer <falconera@users.noreply.github.com>
2017-03-22 13:36:32 -0500

    Converting Option to a class component to fix DOM deselection behavior

--- src/components/Option/Option.js
+++ src/components/Option/Option.js
@@ -1,22 +1,36 @@
 import React from 'react';
 
-function Option({ type, options, value, onChange }) {
-  let size;
-  switch (type) {
-    case 'dropdown':
-      size = 1;
-      break;
-
-    case 'list':
-    default:
-      size = options.length > 2 ? options.length : 2;
+class Option extends React.Component {
+  componentDidUpdate() {
+    if (this.props.value === '') {
+      this.selectElement.selectedIndex = -1;
+    }
   }
 
-  return (
-    <select size={size} value={value} onChange={onChange}>
-      {options.map(o => <option key={o.id} value={o.id}>{o.text}</option>)}
-    </select>
-  );
+  render() {
+    const { type, options, value, onChange } = this.props;
+    let size;
+    switch (type) {
+      case 'dropdown':
+        size = 1;
+        break;
+
+      case 'list':
+      default:
+        size = options.length > 2 ? options.length : 2;
+    }
+
+    return (
+      <select
+        ref={(select) => { this.selectElement = select; }}
+        size={size}
+        value={value}
+        onChange={onChange}
+      >
+        {options.map(o => <option key={o.id} value={o.id}>{o.text}</option>)}
+      </select>
+    );
+  }
 }
 
 Option.propTypes = {
