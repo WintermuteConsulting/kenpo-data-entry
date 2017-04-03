a031a8f9e31b2820dc1c206dcbc4dd116272654b
Andrew Falconer <falconera@users.noreply.github.com>
2017-04-03 14:55:36 -0500

    Removing unused stringifyID file

--- src/stringifyID.js
+++
@@ -1,8 +0,0 @@
-import R from 'ramda';
-
-// Function to stringify raw ObjectIDs from Mongo
-const idLens = R.lensProp('_id');
-const toStringLens = R.lensProp('toHexString');
-const stringifyID = R.over(idLens, id => R.view(toStringLens, id).call(id));
-
-export default stringifyID;
