571d4f2e9f6592689e8506541dd4f417731097e0
Andrew Falconer <falconera@users.noreply.github.com>
2017-06-07 12:24:08 -0500

    Cleaning up unused files

--- src/actions/actions.js
+++
@@ -1,5 +0,0 @@
-export const setCollection = name => ({ type: 'SET_COLLECTION', name });
-export const setItem = id => ({ type: 'SET_ITEM', id });
-export const createItem = () => ({ type: 'CREATE_ITEM', id: Date.now().toString() });
-export const updateSelectedItem = datum => ({ type: 'UPDATE_SELECTED_ITEM', datum });
-export const deleteSelectedItem = () => ({ type: 'DELETE_SELECTED_ITEM' });
